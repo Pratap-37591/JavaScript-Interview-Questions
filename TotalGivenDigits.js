4a4885f238eb9fdeb653602b5615ccd76a9f5dc1
Pratap-37591 <prathaphb1310@gmail.com>
2023-05-28 23:40:57 +0530

    totalgiven digitts in the given data

--- TotalGivenDigits.js
+++ TotalGivenDigits.js
@@ -1,12 +1,12 @@
-// Find out total digits of a given number
+// ? Find out total digits of a given number
 
-// Method 1:
+// ! Method 1:
 
 var digits = 2345632234;
 console.log(Math.floor(Math.log10(digits)) + 1);
 
 
-//Method 2:
+// ! Method 2:
 
 var numbers = 12345;
 console.log(String(numbers).length);
