480e8e3a88061a76dabe1fa67c30fee36b27f948
Pratap-37591 <prathaphb1310@gmail.com>
2023-06-01 10:00:32 +0530

    callback example

---
+++ Callback.js
@@ -0,0 +1,15 @@
+// ? What is the callback function in javaScript?
+
+setTimeout(()=> {
+    console.log("Timer")
+},2000)
+
+function x(y){
+    console.log("x");
+    y();
+}
+
+x(function y(){
+    console.log("Callback");
+})
+
