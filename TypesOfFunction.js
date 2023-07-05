c7065a91f59f92ff8acea03640d7f2d092284bbc
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-05 23:49:12 +0530

    All types of  function in javascript

---
+++ TypesOfFunction.js
@@ -0,0 +1,13 @@
+// All type of functions in javascript
+
+// 1. Function Statement aka Function Declaration
+
+function fullName() {
+  console.log("Pratap H b");
+}
+
+// 2. Function Experssion
+
+var person = function () {
+  console.log("Function Experssion");
+};
