4ccf287c0c4473b3aa7baa0a424ef6d3227bb810
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-07 22:04:43 +0530

    total given digits chnages made

--- TotalGivenDigits.js
+++ TotalGivenDigits.js
@@ -11,3 +11,12 @@ console.log(Math.floor(Math.log10(digits)) + 1);
 var numbers = 12345;
 console.log(String(numbers).length);
 
+// Method 3:
+function CountDigits(){
+    const numberString = String(number).trim().replace(/\D\g, ""/);
+    return numberString.length;
+}
+
+number = 123456;
+const digitCount = CountDigits(number);
+console.log(`The number ${number} has ${digitCount} digits.`);
