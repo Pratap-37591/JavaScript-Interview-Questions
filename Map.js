1010c2787ca693a1ac2f039c435c1843b5045b39
Pratap-37591 <prathaphb1310@gmail.com>
2023-08-04 17:52:26 +0530

    map in javascript

---
+++ Map.js
@@ -0,0 +1,20 @@
+// const persons = [
+//   { firstname: "Malcom", lastname: "Reynolds" },
+//   { firstname: "Kaylee", lastname: "Frye" },
+//   { firstname: "Jayne", lastname: "Cobb" },
+// ];
+
+// function getFullName(item) {
+//   return [item.firstname, item.lastname].join(" ");
+// }
+
+// console.log(persons.map(getFullName));
+
+const arr = [2, 5, 6, 3, 5, 10];
+
+// const output = arr.map((x) => x.toString(2)); // Binary
+
+
+console.log(output);
+
+
