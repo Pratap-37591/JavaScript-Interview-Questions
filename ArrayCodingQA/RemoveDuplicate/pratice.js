cc9ffd724b4e919b0b9eedef8db096d89f99cb69
PratapDigiKart-13 <pratap.shivashankar@digikraftsolutions.com>
2023-11-22 22:18:32 +0530

    chnages made

---
+++ ArrayCodingQA/RemoveDuplicate/pratice.js
@@ -0,0 +1,6 @@
+const obj = { name: "pratap", city: "paries" };
+const obj1 = { name: "shiva", city: "bdvt" };
+// const keys = Object.entries(obj);
+
+const mergeObj = Object.assign(obj, obj1);
+console.log(mergeObj);
